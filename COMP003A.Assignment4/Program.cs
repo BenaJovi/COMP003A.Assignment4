992bcf41e48977c22ddc219402ef65edd41410dc
BenaJovi <xavibenavides@yahoo.com>
2023-02-07 21:51:09 -0800

    Add project files.

---
+++ COMP003A.Assignment4/Program.cs
@@ -0,0 +1,27 @@
+﻿// Author: Jovani Benavides 
+// Course: COMP-003A
+// Purpose: 
+
+
+namespace COMP003A.Assignment4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter a Positive Whole Number:");
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
